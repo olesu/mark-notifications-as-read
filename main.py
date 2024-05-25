732bda645252ae8a4af15280af87e47979b35852
Ole Kristian Sunde <ole.sunde@gmail.com>
2024-05-25 11:34:55 +0200

    hovering around - a little stuck

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ def version():
     print('mark_all_gh_notifications as read, version 0.1.0')
 
 
+def mark_all_notifications_as_read_adapter():
+    print('mark 1')
+    print('mark 2')
+
+
+def mark_all_notifications_as_read(mark_all_notifications_as_read_adapter):
+    mark_all_notifications_as_read_adapter()
+
+
 def run():
     if len(sys.argv) > 1 and sys.argv[1] == '--version':
         version()
         return
 
-    print('mark 1')
-    print('mark 2')
+    mark_all_notifications_as_read(mark_all_notifications_as_read_adapter)
 
 
 if __name__ == '__main__':
