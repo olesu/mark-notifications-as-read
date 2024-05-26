17867b23740f994c50b8f76521ea56b27dc825ee
Ole Kristian Sunde <ole.sunde@gmail.com>
2024-05-26 15:31:15 +0200

    impl complete

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
+from dataclasses import dataclass
+import os
+from typing import Callable
+from github import Github, Auth
 import sys
 
 
-def version():
+def version() -> None:
     print('mark_all_gh_notifications as read, version 0.1.0')
 
 
-def mark_all_notifications_as_read_adapter():
-    print('mark 1')
-    print('mark 2')
+def mark_all_notifications_as_read() -> None:
+    github_token = os.environ.get('GITHUB_TOKEN')
+    if github_token is None:
+        print('GITHUB_TOKEN is required')
+        sys.exit(1)
+
+    github = Github(Auth.Token(github_token))
+    for notification in github.get_user().get_notifications():
+        notification.mark_as_read()
 
 
-def mark_all_notifications_as_read(mark_all_notifications_as_read_adapter):
-    mark_all_notifications_as_read_adapter()
+@dataclass
+class Context:
+    version: Callable[[], None] = None
+    mark_all_notifications_as_read: Callable[[], None] = None
+
+
+context = Context()
 
 
 def run():
     if len(sys.argv) > 1 and sys.argv[1] == '--version':
-        version()
+        context.version()
         return
 
-    mark_all_notifications_as_read(mark_all_notifications_as_read_adapter)
+    context.mark_all_notifications_as_read()
 
 
 if __name__ == '__main__':
+    context.version = version
+    context.mark_all_notifications_as_read = mark_all_notifications_as_read
     run()
--- run_test.py
+++ run_test.py
@@ -1,11 +1,32 @@
+import sys
 import main
 from contextlib import redirect_stdout
 from io import StringIO
 
 
-def test_mark_all_gh_notifications_as_read():
+def test_show_version(monkeypatch):
+    expected = 'a version string'
+
+    monkeypatch.setattr(sys, 'argv', ['main.py', '--version'])
+    monkeypatch.setattr(main.context, 'version', lambda: print(expected))
+
     buffer = StringIO()
     with redirect_stdout(buffer):
         main.run()
 
-    assert buffer.getvalue().strip() == 'mark 1\nmark 2'
+    assert buffer.getvalue().strip() == expected
+
+
+def test_mark_all_gh_notifications_as_read(monkeypatch):
+    expected = 'mark 1\nmark 2'
+
+    monkeypatch.setattr(
+        main.context,
+        "mark_all_notifications_as_read",
+        lambda: print(expected))
+
+    buffer = StringIO()
+    with redirect_stdout(buffer):
+        main.run()
+
+    assert buffer.getvalue().strip() == expected
